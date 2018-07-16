8c771bf6968aa02b6a15f008d0d6d88b06f4cebb
Andrei Sviridov <self.polaris@gmail.com>
2018-07-16 23:02:48 +0200

    remove tons of boilerplate crap and add stub for the gtc badge

---
+++ app/components/GtcBadge/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function GtcBadge() {
+  return (
+    <div>
+      <pre>You have 2 GTC</pre>
+    </div>
+  );
+}
--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -7,15 +7,8 @@
  */
 
 import React from 'react';
-import { Helmet } from 'react-helmet';
 import styled from 'styled-components';
-import { Switch, Route } from 'react-router-dom';
-
-import HomePage from 'containers/HomePage/Loadable';
-import FeaturePage from 'containers/FeaturePage/Loadable';
-import NotFoundPage from 'containers/NotFoundPage/Loadable';
-import Header from 'components/Header';
-import Footer from 'components/Footer';
+import GtcBadge from 'components/GtcBadge';
 
 const AppWrapper = styled.div`
   max-width: calc(768px + 16px * 2);
@@ -29,19 +22,7 @@ const AppWrapper = styled.div`
 export default function App() {
   return (
     <AppWrapper>
-      <Helmet
-        titleTemplate="%s - React.js Boilerplate"
-        defaultTitle="React.js Boilerplate"
-      >
-        <meta name="description" content="A React.js Boilerplate application" />
-      </Helmet>
-      <Header />
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route path="/features" component={FeaturePage} />
-        <Route path="" component={NotFoundPage} />
-      </Switch>
-      <Footer />
+      <GtcBadge />
     </AppWrapper>
   );
 }
